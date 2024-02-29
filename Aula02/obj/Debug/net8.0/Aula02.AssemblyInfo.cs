fffe7bf4ae68154a9bc045800d30cba648e7ff1a
hvortex <hebertms2009@hotmail.com>
2024-02-29 14:07:12 -0300

    atualização aula 02

--- Aula02/obj/Debug/net8.0/Aula02.AssemblyInfo.cs
+++ Aula02/obj/Debug/net8.0/Aula02.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aula02")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cd4497825210aacb60e835a06798bd56fbeb4ace")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a60f9658f4089a34fffc5173ad9af95921dff43e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aula02")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aula02")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
