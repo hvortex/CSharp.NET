24fbd3266faf2cca49eec61bea97a37ff382ce78
hvortex <hebertms2009@hotmail.com>
2024-02-29 14:13:00 -0300

    att aula 2 assembly

--- Aula02/obj/Debug/net8.0/Aula02.AssemblyInfo.cs
+++ Aula02/obj/Debug/net8.0/Aula02.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aula02")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a60f9658f4089a34fffc5173ad9af95921dff43e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fffe7bf4ae68154a9bc045800d30cba648e7ff1a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aula02")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aula02")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
