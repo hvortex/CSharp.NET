e63d4d7ab8164177f4ba233ecec41785cf864ed4
hvortex <hebertms2009@hotmail.com>
2024-03-01 15:27:44 -0300

    Aula 31

---
+++ Aula31/aula31.cs
@@ -0,0 +1,48 @@
+using System;
+
+static public class Jogador{
+    static public int energia;
+    static public bool vivo;
+    static public string nome;
+        static public void iniciar(string n){
+        energia=100;
+        vivo=true;
+        nome=n;
+        }
+
+    static public void info(){
+        Console.WriteLine("Nome do jogador...: {0}",nome);
+        Console.WriteLine("Energia do jogador...: {0}",energia);
+        Console.WriteLine("Estado do jogador...: {0}\n",vivo);
+    }
+}
+class Inimigo{
+    static public bool alerta;
+    public string nome;
+    public Inimigo(string n){
+        alerta=false;
+        nome=n;
+    }
+    public void info(){
+        Console.WriteLine(nome);
+        Console.WriteLine(alerta);
+        Console.WriteLine("----------------");
+    }
+}
+class Aula31{
+    static void Main(){
+
+        Jogador.iniciar("Bruno");
+        Jogador.info();
+
+        Inimigo i1=new Inimigo("Doido");
+        Inimigo i2=new Inimigo("Maluco");
+        Inimigo i3=new Inimigo("Pirado");
+
+        Inimigo.alerta=true;
+        
+        i1.info();
+        i2.info();
+        i3.info();
+    }
+}
