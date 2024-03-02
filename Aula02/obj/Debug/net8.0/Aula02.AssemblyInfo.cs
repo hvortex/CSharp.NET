be2ae88483b1a309136e72146b7274cbb3be51bf
Hebert Magalhães Soares <hebertms2009@hotmail.com>
2024-03-01 23:44:41 -0300

    att 02

--- Aula02/obj/Debug/net8.0/Aula02.AssemblyInfo.cs
+++ Aula02/obj/Debug/net8.0/Aula02.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Aula02")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bd344b10a2e9d1fcd78a3bd1ae6763b7c854a75e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84ccc46913acbfd4d2862eed8ef646d174b6b765")]
 [assembly: System.Reflection.AssemblyProductAttribute("Aula02")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Aula02")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
