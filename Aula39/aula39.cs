1ca763f5010dcd3fdc65726ec80bf4c27b5404da
Hebert Magalhães Soares <hebertms2009@hotmail.com>
2024-03-12 11:30:46 -0300

    Aula39

---
+++ Aula39/aula39.cs
@@ -0,0 +1,39 @@
+using System;
+
+abstract class Veiculo{//classe base abstrata
+    protected int velMaxima;
+    protected int velAtual;
+    protected bool ligado;
+    public Veiculo(){
+        ligado=false;
+        velAtual=0;
+    }
+    public void setLigado(bool ligado){
+        this.ligado=ligado;
+    }
+    public int getVelAtual(){
+        return velAtual;
+    }
+
+    abstract public void aceleração(int mult);
+}
+
+class Carro:Veiculo{
+    public Carro(){
+        velMaxima=120;
+    }
+    override public void aceleração(int mult){
+        velAtual+=10*mult;
+    }
+}
+class Aula39{
+    static void Main(){
+
+        Carro carro1=new Carro();
+
+        carro1.aceleração(1);
+        carro1.aceleração(-1);
+
+        Console.WriteLine(carro1.getVelAtual());
+    }
+}
