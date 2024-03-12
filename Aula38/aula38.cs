a59ae0a4a53e13d9ace9b6026900501518ea6fe5
Hebert Magalhães Soares <hebertms2009@hotmail.com>
2024-03-12 11:11:58 -0300

    Aula38

---
+++ Aula38/aula38.cs
@@ -0,0 +1,38 @@
+using System;
+
+class Base{
+    public Base(){
+        Console.WriteLine("Construtor de classe Base");
+    }
+
+    virtual public void info(){
+    }
+}
+class Derivada1:Base{
+    public Derivada1(){
+        Console.WriteLine("Construtor de classe Derivada 1");
+    }    
+    override public void info(){
+        Console.WriteLine("Derivada 1");
+    }
+}
+
+class Derivada2:Derivada1{
+    public Derivada2(){
+        Console.WriteLine("Construtor de classe Derivada 2");
+    }
+    override public void info(){
+        Console.WriteLine("Derivada 2");
+    }
+}
+
+class Aula38{
+    static void Main(){
+        Base Ref;
+        Derivada1 derivada1=new Derivada1();
+        Derivada2 derivada2=new Derivada2();
+        
+        Ref=derivada2;
+        Ref.info();
+    }
+}
